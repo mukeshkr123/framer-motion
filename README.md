1e68aa8aec8badf1b4de4a4f930e4bedd46e0c1b
mukesh kumar <mkmehta2041@gmail.com>
2024-03-09 14:30:01 +0530

    add first animation

--- README.md
+++ README.md
@@ -20,3 +20,29 @@ export default function App() {
   );
 }
 ```
+
+## FIRST ANIMATION
+
+```tsx
+import { motion } from "framer-motion";
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-white">
+      <motion.button
+        className="bg-black px-3 py-2 rounded-md"
+        animate={{
+          backgroundColor: "#f00",
+          color: "#0f0",
+          // transform: "scale(2) translateY(50px)",
+          scale: 2,
+          y: 50,
+          rotate: 1800,
+        }}
+      >
+        Hello world
+      </motion.button>
+    </div>
+  );
+}
+```
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import { motion } from "framer-motion";
-import { forwardRef } from "react";
-
-const Buttton = forwardRef((props, ref) => {
-  return <button ref={ref}>...</button>;
-});
-
-const MotionButton = motion(Buttton);
 
 export default function App() {
   return (
     <div className="flex items-center justify-center min-h-screen text-white">
-      <motion.button className="bg-black p-3">Hello world</motion.button>
+      <motion.button
+        className="bg-black px-3 py-2 rounded-md"
+        animate={{
+          backgroundColor: "#f00",
+          color: "#0f0",
+          // transform: "scale(2) translateY(50px)",
+          scale: 2,
+          y: 50,
+          rotate: 1800,
+        }}
+      >
+        Hello world
+      </motion.button>
     </div>
   );
 }
