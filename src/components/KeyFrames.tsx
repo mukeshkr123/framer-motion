feba4af4af385c28445c3e140978997a8e43c764
mukesh kumar <mkmehta2041@gmail.com>
2024-03-16 22:26:07 +0530

    added codes

--- src/components/KeyFrames.tsx
+++ src/components/KeyFrames.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+import { motion } from "framer-motion";
 
 export const KeyFrames = () => {
-  return <div>KeyFrames</div>;
+  return (
+    <>
+      <motion.div
+        className="bg-gray-400 w-32 h-32"
+        animate={{
+          scale: [1, 2, 1],
+          rotate: [0, 0, 180, 180, 0],
+          borderRadius: ["0%", "0%", "50%", "0%", "0%"],
+        }}
+        transition={{
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatDelay: 1,
+          duration: 2,
+        }}
+      />
+      <motion.div
+        className="bg-gray-800 w-32 h-32"
+        animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 180, 180, 0],
+          borderRadius: ["0%", "0%", "50%", "0%", "0%"],
+        }}
+        transition={{
+          ease: "easeInOut",
+          repeat: Infinity,
+          repeatDelay: 1,
+          duration: 2,
+          times: [0, 0.25, 0.5, 0.75, 1],
+        }}
+      />
+    </>
+  );
 };
